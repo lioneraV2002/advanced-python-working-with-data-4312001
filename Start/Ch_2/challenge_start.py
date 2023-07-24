2ecbe9deee6ad714f5726031ac4a65fd95a4ab21
k_maham <kamyar_maham1381@yahoo.com>
2023-07-24 19:37:26 +0330

    finished chapter 2

--- Start/Ch_2/challenge_start.py
+++ Start/Ch_2/challenge_start.py
@@ -2,8 +2,20 @@
 # Programming challenge: use advanced data collections on the earthquake data
 
 import json
-
+import collections
 
 # open the data file and load the JSON
 with open("../../30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
+
+# use a default dictionary to categorize each event and count each one
+totals = collections.defaultdict(int)
+for event in data["features"]:
+    totals[event["properties"]["type"]] += 1
+
+for k, v in totals.items():
+    # 15 means to specify the length of the f-string field that k will occupy
+    # if the length is less than 15, then spaces will be added to make up for the difference
+    # if not, the width of the field will automatically expand
+
+    print(f"{k:15}: {v}")
