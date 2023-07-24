176dd9746f5ebe5934c5d35659af1fa268b3befe
k_maham <kamyar_maham1381@yahoo.com>
2023-07-24 17:36:47 +0330

    default dictionary section, done

--- Start/Ch_2/defaultdict.py
+++ Start/Ch_2/defaultdict.py
@@ -2,16 +2,20 @@
 
 from collections import defaultdict
 
-
 # define a list of items that we want to count
 fruits = ['apple', 'pear', 'orange', 'banana',
           'apple', 'grape', 'banana', 'banana']
 
 # TODO: use a dictionary to count each element
-fruitCounter = dict()
+# default dictionary creates a new pair when searching for missing keys in a dictionary
+fruitCounter = defaultdict(int)
+# for fruit in fruits:
+#     fruitCounter[fruit] += 1
 
 # TODO: Count the elements in the list
 for fruit in fruits:
     fruitCounter[fruit] += 1
 
 # TODO: print the result
+for (k, v) in fruitCounter.items():
+    print(k, "=----->", v)
